22798e76f767f3e2a10fcc0227e526701ee27b74
gaoliang851 <851768328@qq.com>
2021-09-26 11:21:23 +0800

    --------【代码更新】----------- 1. 设置项目目录结构

---
+++ Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/View（视图控制器）/Main/GLBaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GLBaseViewController.swift
+//  GLWeiBo_Swift
+//
+//  Created by mac on 2021/9/26.
+//
+
+import UIKit
+
+class GLBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

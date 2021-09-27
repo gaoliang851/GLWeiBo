91cbbf6a7bcd08adc5f21381e46b459880521f87
gaoliang851 <851768328@qq.com>
2021-09-27 17:18:11 +0800

    --------【代码更新】----------- 1. 调整GLMainViewController和GLNavigationController的目录结构 2. 在GLBaseViewController添加空白的tableView

--- Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/View（视图控制器）/Main/GLBaseViewController.swift
+++ Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/View（视图控制器）/Main/GLBaseViewController.swift
@@ -13,6 +13,10 @@ class GLBaseViewController: UIViewController, UIGestureRecognizerDelegate {
     
     lazy var navItem: UINavigationItem = UINavigationItem()
     
+    
+    /// 这里定义成可选的，原因是当用户没有登录是，tableView是不需要创建的
+    var tableView: UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -26,11 +30,23 @@ class GLBaseViewController: UIViewController, UIGestureRecognizerDelegate {
 }
 
 extension GLBaseViewController {
+    
     @objc func setupUI() {
         view.backgroundColor = UIColor.cz_random()
+        setupNavigationBar()
+        setupTableView()
+    }
+    
+    /// 设置导航条的方法
+    fileprivate func setupNavigationBar() {
         navigationBar.items = [navItem]
         //设置navigationbar标题颜色
         navigationBar.titleTextAttributes = [.foregroundColor:UIColor.darkGray]
         view.addSubview(navigationBar)
+    }
+    /// 设置table View
+    fileprivate func setupTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        view.insertSubview(tableView!, belowSubview: navigationBar)
     }
 }

1585d12f7d5f7fd2a487759636cec4dea51a238e
gaoliang <851768328@qq.com>
2021-11-14 15:09:32 +0800

    添加新浪微博后台的基本数据

--- Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/Tools/GLWeiBoCommon.swift
+++ Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/Tools/GLWeiBoCommon.swift
@@ -9,3 +9,11 @@ import Foundation
 
 //MARK: - 全局的通知定义
 let GLUserShouldLoginNotification = "GLUserShouldLoginNotification"
+
+//MARK: - 应用程序信息
+// 应用程序ID
+let GLAppKey = "1805853131"
+// 应用程序加密信息
+let GLAppSecret = "47573d2be75c23541274f33eec28a86d"
+// 回调地址
+let GLRedirectURI = "https://baidu.com"

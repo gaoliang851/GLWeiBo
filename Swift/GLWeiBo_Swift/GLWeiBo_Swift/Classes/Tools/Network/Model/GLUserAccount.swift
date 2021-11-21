0814cf72be88174deee7ad34a277b82956c3c295
gaoliang <851768328@qq.com>
2021-11-21 15:08:07 +0800

    抽取文件名，实现从磁盘加载用户信息

--- Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/Tools/Network/Model/GLUserAccount.swift
+++ Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/Tools/Network/Model/GLUserAccount.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+fileprivate let fileName :NSString = "user_account.json"
+
 @objcMembers
 class GLUserAccount: NSObject {
     /// 访问令牌
@@ -22,14 +24,32 @@ class GLUserAccount: NSObject {
     /// 过期日期
     var expiresDate: Date?
     
+    override init() {
+        super.init()
+        //从磁盘读取账户数据
+        guard let path = fileName.cz_appendDocumentDir(),
+              // 将磁盘数据二进制读取
+              let data = NSData(contentsOfFile: path),
+              // 将二进制转为字典
+              let dict = try? JSONSerialization.jsonObject(with: data as Data, options: []) as? [String:Any] ?? [:] else {
+                  return
+        }
+        yy_modelSet(with: dict)
+        print("account info : \(self)")
+    }
+    
     /// 保存账号数据
     func saveAccount() {
+        // 将模型转为字典
         var dict = self.yy_modelToJSONObject() as? [String:Any] ?? [:]
+        // 将字典中的expiress_in移除
         dict.removeValue(forKey: "expires_in")
+        // 字典转data
         guard let data = try? JSONSerialization.data(withJSONObject: dict, options: [.prettyPrinted]),
-        let filePath = ("user_account.json" as NSString).cz_appendDocumentDir() else {
+        let filePath = fileName.cz_appendDocumentDir() else {
             return
         }
+        // 写入沙盒
         let result = (data as NSData).write(toFile: filePath, atomically: true)
         logi("filePath is \(filePath),result = \(result)")
     }

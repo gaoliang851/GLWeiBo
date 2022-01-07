ea0f30f43a1a21c376ee68baa7b73fd4e128155b
51wleinkQuickLogin <851768328@qq.com>
2022-01-07 16:23:24 +0800

    添加scrollview连线，添加6个按钮到view。修改GLComposeTypeButton背景色为透明

--- Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/View（视图控制器）/Main/ComposeTypeView/GLComposeTypeView.swift
+++ Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/View（视图控制器）/Main/ComposeTypeView/GLComposeTypeView.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class GLComposeTypeView: UIView {
+    @IBOutlet weak var scrollView: UIScrollView!
     
     lazy var buttonsInfo = [["imageName":"tabbar_compose_idea","title": "文字"],
                             ["imageName":"tabbar_compose_photo","title": "照片/视频"],
@@ -24,7 +25,12 @@ class GLComposeTypeView: UIView {
     class func composeTypeView() -> GLComposeTypeView {
         let nib = UINib(nibName: "GLComposeTypeView", bundle: nil)
         
+        /// 这个方法里面调用的 awakeForNib,
         let v = nib.instantiate(withOwner: nil, options: nil)[0] as! GLComposeTypeView
+        
+        v.frame = UIScreen.main.bounds
+        
+        v.setupUI()
         
         return v
     }
@@ -34,15 +40,17 @@ class GLComposeTypeView: UIView {
             window.isKeyWindow
          }.first?.rootViewController
         
-        self.frame = UIScreen.main.bounds
-        
         vc?.view.addSubview(self)
     }
     
+    /*
     override func awakeFromNib() {
         super.awakeFromNib()
+        // 调用awkeFormNib时，此时控件以及子控件的frame、bounds还都是xib中的尺寸
+        // 所以调用setupUI()中去设置控件大小，如果使用freme
         setupUI()
     }
+     */
     
     @objc func clickBtn() {
         logi("点击了按钮")
@@ -62,5 +70,59 @@ private extension GLComposeTypeView {
         //addSubview(btn)
         
 //        addSubview()
+        
+        //强制更新约束
+        layoutIfNeeded()
+        
+        let v = UIView(frame: scrollView.bounds)
+        
+        addButtons(v: v, idx: 0)
+        
+        scrollView.addSubview(v)
+        
+    }
+    
+    
+    /// 向指定的view添加按钮
+    /// - Parameters:
+    ///   - v: 要添加按钮的view
+    ///   - idx: 添加按钮从buttonsInfo开始的索引
+    private func addButtons(v: UIView, idx: Int) {
+        
+        /// 循环创建6个按钮
+        let count = 6
+        for i in idx..<(idx + count) {
+            if i >= buttonsInfo.count { break }
+            /// 获取图片名称和title
+            let dict = buttonsInfo[i]
+            guard let imageName = dict["imageName"],
+                  let title = dict["title"] else {
+                      continue
+            }
+            // 创建按钮
+            let btn = GLComposeTypeButton.composeTypeButton(imageName: imageName, title: title)
+            // 添加到视图
+            v.addSubview(btn)
+            // 添加监听方法
+            btn.addTarget(self, action: #selector(clickBtn), for: .touchUpInside)
+        }
+        /// 定义常量
+        // 按钮尺寸
+        let btnSize = CGSize(width: 100, height: 100)
+        // 按钮间距
+        let margin = (v.bounds.width - 3 * btnSize.width) / 4
+        
+        // 开始布局, i是索引,btn是元素
+        for (i,btn) in v.subviews.enumerated() {
+            // 行
+            //let row = CGFloat(i / 3)
+            // 列
+            let col = CGFloat(i % 3)
+            // x,
+            let X = margin + (margin + btnSize.width) * col
+            let Y = i <= 2 ? 0 : (v.bounds.height - btnSize.height)
+            let rect = CGRect(x: X, y: Y, width: btnSize.width, height: btnSize.height)
+            btn.frame = rect
+        }
     }
 }

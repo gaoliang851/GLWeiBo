8da7faf7667dc67531c6fc72e76b10d5bb75fe83
51wleinkQuickLogin <851768328@qq.com>
2022-01-21 20:08:06 +0800

    35-按照整页设置 cell 的布局

--- Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/Tools/Emotion/View/CZEmotictionLayout.swift
+++ Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/Tools/Emotion/View/CZEmotictionLayout.swift
@@ -11,9 +11,23 @@ class CZEmotictionLayout: UICollectionViewFlowLayout {
     override func prepare() {
         super.prepare()
         
+        
+        
         // 设定滚动方向
         // 水平方向滚动, cell 垂直方向布局
         // 垂直方向滚动, cell 水平方向布局
-        scrollDirection = .horizontal
+        // xib中也可设置
+        // scrollDirection = .horizontal
+        
+        // 在此方法中，collectionView 的大小已经确定
+        guard let collectionView = collectionView else {
+                return
+        }
+        
+        itemSize = collectionView.bounds.size
+        // 行间距 = 0, xib中也可设置
+        //minimumLineSpacing = 0
+        // 水平间距 = 0，xib中也可设置
+        //minimumInteritemSpacing = 0
     }
 }

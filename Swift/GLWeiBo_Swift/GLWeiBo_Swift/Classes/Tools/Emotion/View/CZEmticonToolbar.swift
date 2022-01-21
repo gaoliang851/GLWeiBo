64df253eb8b4d18b97bb9acfdd0a57575758df13
51wleinkQuickLogin <851768328@qq.com>
2022-01-21 15:39:46 +0800

    30-添加工具栏按钮／以及简单的布局

--- Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/Tools/Emotion/View/CZEmticonToolbar.swift
+++ Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/Tools/Emotion/View/CZEmticonToolbar.swift
@@ -8,13 +8,45 @@
 import UIKit
 
 class CZEmticonToolbar: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override func awakeFromNib() {
+        setupUI()
     }
-    */
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let count = subviews.count
+        let width = bounds.width / CGFloat(count)
+        let rect = CGRect(x: 0, y: 0, width: width, height: bounds.height)
+        
+        // 布局按钮
+        for (i,v) in subviews.enumerated() {
+            v.frame = rect.offsetBy(dx: width * CGFloat(i), dy: 0)
+        }
+    }
+}
 
+private extension CZEmticonToolbar {
+    func setupUI() {
+        // 获取表情管理器
+        let manager = CZEmoticonManager.shared
+        // 遍历分组名称
+        for p in manager.packages {
+            // 初始化按钮
+            let button = UIButton(type: .custom)
+            
+            button.setTitle(p.groupName, for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+            
+            button.setTitleColor(.white, for: .normal)
+            button.setTitleColor(.darkGray, for: .selected)
+            button.setTitleColor(.darkGray, for: .highlighted)
+            button.backgroundColor = .blue
+            
+            button.sizeToFit()
+            
+            addSubview(button)
+        }
+    }
 }

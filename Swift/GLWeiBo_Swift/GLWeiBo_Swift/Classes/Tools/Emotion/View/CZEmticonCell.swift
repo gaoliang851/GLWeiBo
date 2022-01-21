914f9274a97dcc539c3863a8dad85e56f4021014
51wleinkQuickLogin <851768328@qq.com>
2022-01-21 18:12:32 +0800

    33-使用 class 和 nib 注册可重用cell，绑定假数据

---
+++ Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/Tools/Emotion/View/CZEmticonCell.swift
@@ -0,0 +1,13 @@
+//
+//  CZEmticonCell.swift
+//  day11-001-表情键盘
+//
+//  Created by gaoliang on 2022/1/21.
+//
+
+import UIKit
+
+class CZEmticonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
--- Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/Tools/Emotion/View/CZEmticonInputView.swift
+++ Swift/GLWeiBo_Swift/GLWeiBo_Swift/Classes/Tools/Emotion/View/CZEmticonInputView.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+fileprivate let cellId = "cellId"
+
 class CZEmticonInputView: UIView {
     
     @IBOutlet weak var CZEmticonToolbar: UIView!
@@ -19,4 +21,29 @@ class CZEmticonInputView: UIView {
         let v = nib.instantiate(withOwner: nil, options: nil)[0] as! CZEmticonInputView
         return v
     }
+    
+    override func awakeFromNib() {
+        let nib = UINib(nibName: "CZEmticonCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: cellId)
+    }
+}
+
+extension CZEmticonInputView : UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        CZEmoticonManager.shared.packages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CZEmticonCell
+        
+        cell.label.text = "\(indexPath.section) - \(indexPath.item)"
+    
+        return cell
+    }
+    
 }

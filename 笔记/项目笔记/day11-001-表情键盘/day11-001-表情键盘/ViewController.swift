8a2946b015a127a9b4e64626fccfd8345bae2bc6
51wleinkQuickLogin <851768328@qq.com>
2022-01-24 10:29:03 +0800

    52-删除按钮和 emoji 字符串

--- 笔记/项目笔记/day11-001-表情键盘/day11-001-表情键盘/ViewController.swift
+++ 笔记/项目笔记/day11-001-表情键盘/day11-001-表情键盘/ViewController.swift
@@ -15,8 +15,23 @@ class ViewController: UIViewController {
         self?.insertEmoticon(em: emticon)
     }
     
+    
+    /// 向文本中插入表情 [图文混排]
+    /// - Parameter em: 选入的表情, nil 表示 删除
     private func insertEmoticon(em: CZEmoticon?) {
         
+        // 1. em == nil , 则说明是删除按钮
+        guard let em = em else {
+            // 删除文本，有选中的文本删除选中的，没有选中的，删除光标前的一个文本
+            textView.deleteBackward()
+            return
+        }
+        
+        // 2. 如果是emoji, 直接插入
+        if let emoji = em.emoji,
+            let textRange = textView.selectedTextRange {
+            textView.replace(textRange, withText: emoji)
+        }
     }
     
     override func viewDidLoad() {
